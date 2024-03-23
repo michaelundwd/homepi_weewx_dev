58d5b7824c10621acc15e4fb3b0d4ec474b71f86
michaelundwd <45148992+michaelundwd@users.noreply.github.com>
2024-03-23 18:08:14 +0000

    remove TZ setting; its in dockerfile

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -16,12 +16,12 @@ if [ "$1" = "--version" ]; then
 fi
 
 if [ "$(id -u)" = 0 ]; then
-  echo set timezone using environment
-  ln -snf /usr/share/zoneinfo/"${TIMEZONE:-UTC}" /etc/localtime
-  # start the syslog daemon as root
+  #echo set timezone using environment
+  #ln -snf /usr/share/zoneinfo/"${TIMEZONE:-UTC}" /etc/localtime
+  echo start the syslog daemon as root
   /sbin/syslogd -n -S -O - &
   if [ "${WEEWX_UID:-weewx}" != 0 ]; then
-    echo drop privileges and restart this script
+    # drop privileges and restart this script
     echo "Switching uid:gid to ${WEEWX_UID:-weewx}:${WEEWX_GID:-weewx}"
     gosu "${WEEWX_UID:-weewx}:${WEEWX_GID:-weewx}" "$(readlink -f "$0")" "$@"
     exit 0
