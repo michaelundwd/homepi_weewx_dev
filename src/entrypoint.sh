e850d5180255f737aa54b794dc40378e830f087e
michaelundwd <45148992+michaelundwd@users.noreply.github.com>
2024-03-23 17:43:29 +0000

    deleted lines

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # all code below here to the final fi is skipped for testing a highly compact entrypoint.sh
-if [[ ]]; then
+#if [[ ]]; then
 
 set -o nounset
 set -o errexit
@@ -15,31 +15,9 @@ if [ "$1" = "--version" ]; then
   exit 0
 fi
 
-if [ "$(id -u)" = 0 ]; then
-  # set timezone using environment
-  ln -snf /usr/share/zoneinfo/"${TIMEZONE:-UTC}" /etc/localtime
-  # start the syslog daemon as root
-  /sbin/syslogd -n -S -O - &
-  if [ "${WEEWX_UID:-weewx}" != 0 ]; then
-    # drop privileges and restart this script
-    echo "Switching uid:gid to ${WEEWX_UID:-weewx}:${WEEWX_GID:-weewx}"
-    gosu "${WEEWX_UID:-weewx}:${WEEWX_GID:-weewx}" "$(readlink -f "$0")" "$@"
-    exit 0
-  fi
-fi
+#deleted lines
 
-copy_default_config() {
-  # create a default configuration on the data volume
-  echo "Creating a configration file on the container data volume."
-  cp weewx.conf "${CONF_FILE}"
-  echo "The default configuration has been copied."
-  # Change the default location of the SQLITE database to the volume
-  echo "Setting SQLITE_ROOT to the container volume."
-  sed -i "s/SQLITE_ROOT =.*/SQLITE_ROOT = \/data/g" "${CONF_FILE}"
-}
-
-
-fi
+#fi
 # this is the end of the code to be skipped
 
 #	copy belchertown.py from /data/bin/user to /home/weewx/bin/user/belchertown.py
